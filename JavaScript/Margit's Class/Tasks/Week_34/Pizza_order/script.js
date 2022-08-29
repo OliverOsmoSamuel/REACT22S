a72a5f913f931bf010cc7278c2173545e9848795
OliverOsmoSamuel <oliver.janhunen@gmail.com>
2022-08-29 14:58:51 +0300

    first commit, html done, css almost done, js not yet written

---
+++ JavaScript/Margit's Class/Tasks/Week_34/Pizza_order/script.js
@@ -0,0 +1,8 @@
+let priceCalculator = () => {};
+
+let orderSummary = () => {
+  let price = document.querySelector("#name").value,
+    size = document.querySelector("#size").value,
+    toppings = document.querySelector("#toppings").value,
+    delivery = document.querySelector("#delivery").value;
+};
