325b757c6f0178d37a891f190dada8fd66f7e7a1
OliverOsmoSamuel <oliver.janhunen@gmail.com>
2022-09-11 22:43:31 +0300

    added js

--- JavaScript/Margit's Class/Tasks/Week_36/Car_Database/script.js
+++ JavaScript/Margit's Class/Tasks/Week_36/Car_Database/script.js
@@ -1,17 +1,39 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    fetchData();
-  },
-  false
-);
+const form = document.querySelector("form");
+const database = document.querySelector("#database");
+const licenceNumber = document.querySelector("#licence-number");
+const carMaker = document.querySelector("#car-maker");
+const carModel = document.querySelector("#car-model");
+const carOwner = document.querySelector("#car-owner");
+const carPrice = document.querySelector("#car-price");
+const carColor = document.querySelector("#car-color");
 
-async function fetchData() {
-  const response = await fetch("http://localhost:5500/data.json");
-  const data = await response.json();
-  console.log(data);
+const addData = (event) => {
+  event.preventDefault();
+  let car = [];
 
-  data.forEach((car) => {
-    car.forEach((data) => {});
+  car.push(licenceNumber.value);
+  car.push(carMaker.value);
+  car.push(carModel.value);
+  car.push(carOwner.value);
+  car.push(carPrice.value);
+  car.push(carColor.value);
+
+  car.forEach((data) => {
+    let newP = document.createElement("p");
+    let newContent = document.createTextNode(`${data}`);
+    newP.appendChild(newContent);
+    newP.classList.add("database__p");
+    database.appendChild(newP);
   });
-}
+
+  form.reset();
+};
+
+const resetData = () => {
+  let databaseP = document.querySelectorAll(".database__p");
+  databaseP.forEach((data) => {
+    data.remove();
+  });
+};
+
+form.addEventListener("submit", addData);
