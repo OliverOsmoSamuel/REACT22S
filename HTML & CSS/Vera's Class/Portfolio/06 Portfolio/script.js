acb945cfe0bfff48ac8013befc6275d2f2970a1a
OliverOsmoSamuel <oliver.janhunen@gmail.com>
2022-09-17 11:14:58 +0300

    improved js, no scroll on mobile navigation, improved to top button, simplified footer

--- HTML & CSS/Vera's Class/Portfolio/06 Portfolio/script.js
+++ HTML & CSS/Vera's Class/Portfolio/06 Portfolio/script.js
@@ -1,13 +1,23 @@
+const toggleButton = document.querySelector("#navbar__links-mobile");
+const navbar = document.querySelector("#navbar");
+const toTopButton = document.querySelector("#to-top");
+const body = document.querySelector("body");
+
 let navToggle = () => {
-  let toggleButton = document.getElementById("navbar__links-mobile");
   toggleButton.classList.toggle("navbar__links-mobile--open");
+  body.classList.toggle("scroll--disable");
 };
 
 window.addEventListener("scroll", function () {
-  let scrollPosition = window.pageYOffset;
-  if (scrollPosition >= 1) {
-    document.getElementById("navbar").classList.add("navbar--scrolled");
-  } else {
-    document.getElementById("navbar").classList.remove("navbar--scrolled");
-  }
+  const scrollPosition = window.pageYOffset;
+  scrollPosition >= 201
+    ? toTopButton.classList.add("to-top--visible")
+    : toTopButton.classList.remove("to-top--visible");
+});
+
+window.addEventListener("scroll", function () {
+  const scrollPosition = window.pageYOffset;
+  scrollPosition >= 1
+    ? navbar.classList.add("navbar--scrolled")
+    : navbar.classList.remove("navbar--scrolled");
 });
